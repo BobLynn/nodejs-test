23013eb22082d74adc9cfdd05f5c1112c9eb48f3
BobLynn <476127332@qq.com>
2023-03-02 04:01:31 +0800

    initial npm updated

--- server.js
+++ server.js
@@ -11,19 +11,22 @@ if(req.url == '/'){
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('mainPage')
 }
-
+//GET: 從API取得資料
 else if(req.url == '/property-content' && req.method == 'GET'){
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end("{'property name': 'PC Monitor', 'serial number': '3101010-123456567', 'professor': 'Shih-An, Lee'}")
 }
+//POST: 傳送資料以建立資料庫的資料
 else if(req.url == '/property-content' && req.method == 'POST'){
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end('GOT PROPERTY DATA!');
 }
+//PUT: 傳送資料以更新資料庫的資料
 else if(req.url == '/property-content' && req.method == 'PUT'){
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end('PROPERTY DATA UPDATED!');
 }
+//DELETE: 刪除資料
 else if(req.url == '/property-content' && req.method == 'DELETE'){
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end('PROPERTY DATA DELETED!');
