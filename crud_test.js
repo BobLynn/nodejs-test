82480c4e1d3f4f112f173cd02972942931e535ee
Bob Lynn <476127332@qq.com>
2023-03-07 19:56:51 +0800

    changed

--- crud_test.js
+++ crud_test.js
@@ -1,6 +1,8 @@
 const exp = require("constants")
 const http = require("http")    //http原生模組
 const sqlite3 = require('sqlite3')  //SQLite
+const utf8 = require("utf8")
+
 const port_1 = 3000
 const port_2 = 8000
 
@@ -14,22 +16,24 @@ if(req.url == '/'){
 //GET: 從API取得資料
 else if(req.url == '/property-content' && req.method == 'GET'){
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end("{'property name': 'PC Monitor', 'serial number': '3101010-123456567', 'professor': 'Shih-An, Lee'}")
+    res.end("{'property name': 'PC Monitor', \
+    'serial number': '3101010-123456567', \
+    'professor': 'Shih-An, Lee'}")
 }
 //POST: 傳送資料以建立資料庫的資料
 else if(req.url == '/property-content' && req.method == 'POST'){
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end('GOT PROPERTY DATA!');
+    res.end('取得資料庫資料！ GOT PROPERTY DATA!')
 }
 //PUT: 傳送資料以更新資料庫的資料
 else if(req.url == '/property-content' && req.method == 'PUT'){
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end('PROPERTY DATA UPDATED!');
+    res.end('財產資料已更新！ PROPERTY DATA UPDATED!')
 }
 //DELETE: 刪除資料
 else if(req.url == '/property-content' && req.method == 'DELETE'){
     res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end('PROPERTY DATA DELETED!');
+    res.end('財產資料已刪除！ PROPERTY DATA DELETED!')
 }
 })
 
