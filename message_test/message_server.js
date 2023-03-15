00a0d7c62e1e35596ddb61a513c042f9b2fb3a5f
BobLynn <476127332@qq.com>
2023-03-16 04:22:12 +0800

    successfully made 'message_test' section

--- message_test/message_server.js
+++ message_test/message_server.js
@@ -1,34 +1,35 @@
-//import express 和 ws 套件
+// import library
 const express = require('express')
-const SocketServer = require('ws').Server
-const PORT = 3000 //指定 port
+const ServerSocket = require('ws').Server   // 引用 Server
 
-//創建 express 物件，綁定監聽  port , 設定開啟後在 console 中提示
-const server = express().listen(PORT, () => {
-    console.log('Listening on %d', PORT)
-})
-//將 express 交給 SocketServer 開啟 WebSocket 的服務
-const wss = new SocketServer({ server })
-//當有 client 連線成功時
-wss.on('connection', ws => {
-  console.log('Client connected')
-  // 當收到client消息時
+// 指定一個 port
+const PORT = 8080
+
+// 建立 express 物件並用來監聽 8080 port
+const server = express()
+    .listen(PORT, () => console.log('[Server] Listening on https://localhost:%d', PORT))
+
+// 建立實體，透過 ServerSocket 開啟 WebSocket 的服務
+const wss = new ServerSocket({ server })
+
+// Connection opened
+wss.on('connection', (ws, req) => {
+  ws.id = req.headers['sec-websocket-key'].substring(0, 8)
+  ws.send(`[Client ${ws.id} is connected!]`)
+
+  // Listen for messages from client
   ws.on('message', data => {
-    // 收回來是 Buffer 格式、需轉成字串
-    data = data.toString()  
-    console.log(data) // 可在 terminal 看收到的訊息
-
-    /// 發送消息給client 
-    ws.send(data)
-
-    /// 發送給所有client： 
-    let clients = wss.clients  //取得所有連接中的 client
-    clients.forEach(client => {
-        client.send(data)  // 發送至每個 client
-    })
+      console.log('[Message from client] data: ', data)
+      // Get clients who has connected
+      let clients = wss.clients
+      // Use loop for sending messages to each client
+      clients.forEach(client => {
+          client.send(`${ws.id}: ` + data)
+      })
   })
-  // 當連線關閉
+
+  // Connection closed
   ws.on('close', () => {
-    console.log('Close connected')
+      console.log('[Close connected]')
   })
 })
